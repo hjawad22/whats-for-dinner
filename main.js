5435bde2430adfa9abb342d7e5cc0f1994b041a2
Houda Jawad <houdajawad@gmail.com>
2023-02-18 15:35:51 -0800

    Clean up the code

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var selectSide = document.querySelector("#side");
 var letsCookButton = document.querySelector(".lets-cook-button");
 var foodOutput = document.querySelector(".food-selection")
 var foodOutputText = document.querySelector(".selection-text");
-var crockPotImg = document.querySelector(".crockpot-img")
+var crockPotImg = document.querySelector(".crockpot-img");
 
 // eventListeners
 letsCookButton.addEventListener("click", getDish );
@@ -12,11 +12,11 @@ letsCookButton.addEventListener("click", getDish );
 function getDish() {
    if (document.getElementById("side").checked === true) {
   foodOutputText.innerText = `${getRandomIndex(sides)}!`
- } else if (document.getElementById("main-dish").checked === true) {
+   } else if (document.getElementById("main-dish").checked === true) {
   foodOutputText.innerHTML = `${getRandomIndex(mains)}!`
- } else if (document.getElementById("dessert").checked === true) {
+   } else if (document.getElementById("dessert").checked === true) {
   foodOutputText.innerHTML = `${getRandomIndex(desserts)}!`
-} else if (document.getElementById("entire-meal").checked === true) {
+   } else if (document.getElementById("entire-meal").checked === true) {
   foodOutputText.innerHTML = `${getRandomIndex(sides)}, ${getRandomIndex(mains)}, ${getRandomIndex(desserts)}!`
 }
 hideCrockPot()
@@ -27,14 +27,14 @@ function getRandomIndex(array) {
 }
 
 function hideCrockPot() {
-crockPotImg.style.visibility = "hidden"
-foodOutput.style.visibility = "visible"
+  crockPotImg.style.visibility = "hidden";
+  foodOutput.style.visibility = "visible";
 }
 
 // Arrays
-var sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"]
-var mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka"]
-var desserts = ["Apple Pie","Lemon Meringue Pie","Black Forest Cake"]
+var sides = ["Miso Glazed Carrots","Coleslaw","Garden Salad"];
+var mains = ["Spaghetti and Meatballs","Pineapple Chicken","Shakshuka"];
+var desserts = ["Apple Pie","Lemon Meringue Pie","Black Forest Cake"];
    
   
 
